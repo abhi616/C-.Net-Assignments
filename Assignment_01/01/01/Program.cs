04ae5dae9f96dbf2a6d5b6fb3c7aae7199f9af84
abhi616 <ay891241@gmail.com>
2022-03-19 14:31:06 +0530

    Assignment 01

---
+++ Assignment_01/01/01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 0;
+
+            Up:
+            Console.WriteLine(" Enter Number : ");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            if (Num <= 0)
+            {
+                Console.WriteLine("Plaese Enter Positive Number.\n");     
+                goto Up;
+            }
+            else
+            {
+                int Temp = Num;
+
+                for (int i = 1; i <= Num; i++)
+                {
+                    for (int j = 1; j <= Temp*2; j++)
+                    {
+                        Console.Write(Temp);
+                        if (j % 2 == 0)
+                        {
+                            Temp--;
+                            Console.WriteLine();
+                        }
+                        
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
